ef219ad3126e1b8e28eb358d24405ac54124dae6
navneeth <kaur.navneeth@gmail.com>
2018-08-12 17:17:15 -0400

    added

--- src/test/java/com/virtualpairprogrammers/FleetmanApplicationTests.java
+++ src/test/java/com/virtualpairprogrammers/FleetmanApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class FleetmanApplicationTests {
 
-	//@Test
+	@Test
 	public void contextLoads() {
 	}
 
